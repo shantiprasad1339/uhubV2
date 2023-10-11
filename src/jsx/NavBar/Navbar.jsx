439ba9519c780ee6b959e347f20c9d459fd7d1f8
ahitgitlab <ahitpvtltd@gmail.com>
2023-10-11 11:43:35 +0530

    Sidebar changes

--- src/jsx/NavBar/Navbar.jsx
+++ src/jsx/NavBar/Navbar.jsx
@@ -10,7 +10,8 @@ import { NavLink } from "react-router-dom";
 import SignUp from "../SignUp/sing-up";
 import Login from "./../login/login";
 import MenuIcon from "@mui/icons-material/Menu";
-
+import CloseIcon from "@mui/icons-material/Close";
+import Collapsible from "react-collapsible";
 function Navbar() {
   const [isOpen, setIsopen] = useState(false);
 
@@ -41,7 +42,7 @@ function Navbar() {
               </div>
               <marquee behavior="" direction="">
                 {" "}
-                Simply dummy text of the printing and typesetting
+                Simply dummy text of the printing and typesetting.
               </marquee>
             </div>
           </div>
@@ -60,63 +61,263 @@ function Navbar() {
           <div className="haburgerDiv">
             <MenuIcon onClick={ToggleSidebar} />
           </div>
-          <DropdownBtn name={"Collage"} />
-          <DropdownBtn name={"Stream"} />
-          <DropdownBtn name={"Services"} />
-          <DropdownBtn name={"Blog"} />
-          <DropdownBtn name={"Products"} />
-          <DropdownBtn name={"Contact"} />
-          <DropdownBtn name={"Counselling"} />
+
+          <div class="dropdown">
+            <button
+              class=" dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              College
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="#">
+                Action
+              </a>
+              <a class="dropdown-item" href="#">
+                Another action
+              </a>
+              <a class="dropdown-item" href="#">
+                Something else here
+              </a>
+            </div>
+          </div>
+
+          <div class="dropdown">
+            <button
+              class=" dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Stream
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="#">
+                Action
+              </a>
+              <a class="dropdown-item" href="#">
+                Another action
+              </a>
+              <a class="dropdown-item" href="#">
+                Something else here
+              </a>
+            </div>
+          </div>
+
+          <div class="dropdown">
+            <button
+              class=" dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Services{" "}
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="#">
+                Action
+              </a>
+              <a class="dropdown-item" href="#">
+                Another action
+              </a>
+              <a class="dropdown-item" href="#">
+                Something else here
+              </a>
+            </div>
+          </div>
+
+          <div class="dropdown">
+            <button
+              class=" dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Blog
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="#">
+                Action
+              </a>
+              <a class="dropdown-item" href="#">
+                Another action
+              </a>
+              <a class="dropdown-item" href="#">
+                Something else here
+              </a>
+            </div>
+          </div>
+
+          <div class="dropdown">
+            <button
+              class=" dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Products
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="#">
+                Action
+              </a>
+              <a class="dropdown-item" href="#">
+                Another action
+              </a>
+              <a class="dropdown-item" href="#">
+                Something else here
+              </a>
+            </div>
+          </div>
+
+          <div class="dropdown">
+            <button
+              class=" dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+              
+            >
+              Contacts
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="#">
+                Action
+              </a>
+              <a class="dropdown-item" href="#">
+                Another action
+              </a>
+              <a class="dropdown-item" href="#">
+                Something else here
+              </a>
+            </div>
+          </div>
+          <div className="counselling">
+            <div class="dropdown">
+              <button
+                class=" dropdown-toggle"
+                type="button"
+                id="dropdownMenuButton"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Counselling
+              </button>
+              <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" href="#">
+                  Action
+                </a>
+                <a class="dropdown-item" href="#">
+                  Another action
+                </a>
+                <a class="dropdown-item" href="#">
+                  Something else here
+                </a>
+                <a class="dropdown-item" href="#">
+                  Another action
+                </a>
+                <a class="dropdown-item" href="#">
+                  Another action
+                </a>
+                <a class="dropdown-item" href="#">
+                  Another action
+                </a>
+                <a class="dropdown-item" href="#">
+                  Another action
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
-      
-        
-          
-            
-           
-             
-       
-        <div className={`sidebar ${isOpen == true ? "active" : ""}`}>
-          <div className="sd-header">
-            <h4 className="mb-0">Sidebar Header</h4>
-            <MenuIcon onClick={ToggleSidebar} />
-            
-          </div>
-          <div className="sd-body">
-            <ul>
-              <li>
-                <a className="sd-link">Menu Item 1</a>
-              </li>
-              <li>
-                <a className="sd-link">Menu Item 2</a>
-              </li>
-              <li>
-                <a className="sd-link">Menu Item 3</a>
-              </li>
-              <li>
-                <a className="sd-link">Menu Item 4</a>
-              </li>
-              <li>
-                <a className="sd-link">Menu Item 5</a>
-              </li>
-              <li>
-                <a className="sd-link">Menu Item 6</a>
-              </li>
-              <li>
-                <a className="sd-link">Menu Item 7</a>
-              </li>
-              <li>
-                <a className="sd-link">Menu Item 8</a>
-              </li>
-            </ul>
-          </div>
+      <div className={`sidebar ${isOpen == true ? "active" : ""}`}>
+        <div className="sd-header">
+          <h4 className="mb-0">Universty Hub</h4>
+          <CloseIcon onClick={ToggleSidebar} />
         </div>
-        <div
-          className={`sidebar-overlay ${isOpen == true ? "active" : ""}`}
-          onClick={ToggleSidebar}
-        ></div>
-      
+        <div className="sd-body">
+          <ul>
+            <li></li>
+            <li>
+            <Collapsible trigger="Start here" className="sd-link">
+                <ul>
+                  <li>item 1</li>
+                  <li>item 1</li>
+                  <li>item 1</li>
+                </ul>
+              </Collapsible>
+            </li>
+            <li>
+            <Collapsible trigger="Start here" className="sd-link">
+                <ul>
+                  <li>item 1</li>
+                  <li>item 1</li>
+                  <li>item 1</li>
+                </ul>
+              </Collapsible>
+            </li>
+            <li>
+              
+            </li>
+            <li>
+            <Collapsible trigger="Start here" className="sd-link">
+                <ul>
+                  <li>item 1</li>
+                  <li>item 1</li>
+                  <li>item 1</li>
+                </ul>
+              </Collapsible>
+            </li>
+            <li>
+            <Collapsible trigger="Start here" className="sd-link">
+                <ul>
+                  <li>item 1</li>
+                  <li>item 1</li>
+                  <li>item 1</li>
+                </ul>
+              </Collapsible>
+            </li>
+            <li>
+            <Collapsible trigger="Start here" className="sd-link">
+                <ul>
+                  <li>item 1</li>
+                  <li>item 1</li>
+                  <li>item 1</li>
+                </ul>
+              </Collapsible>
+            </li>
+            <li>
+              <Collapsible trigger="Start here" className="sd-link">
+                <ul>
+                  <li>item 1</li>
+                  <li>item 1</li>
+                  <li>item 1</li>
+                </ul>
+              </Collapsible>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div
+        className={`sidebar-overlay ${isOpen == true ? "active" : ""}`}
+        onClick={ToggleSidebar}
+      ></div>
     </>
   );
 }
